cd6943a9b918a5c66b02dc707e5d96758fb1454e
esmayl <esmayl@jellyfishdm.com>
2024-06-04 17:34:22 +0200

    Fixed windows compilation

--- src/server.cpp
+++ src/server.cpp
@@ -4,9 +4,12 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
+
 #ifdef _WIN64
-    #include <winsock2.h>
-#include <wspiapi.h>
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
 
 #else
     #include <sys/socket.h>
@@ -22,32 +25,77 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
+#ifdef _WIN64
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"Error at WSAStartup()\n");
+        return 1;
+    }
+
+    // Uncomment this block to pass the first stage
+    SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ListenSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
+#else
   // Uncomment this block to pass the first stage
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
   }
-
+#endif
   
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
 #ifdef _WIN64
  const char reuse = SO_KEEPALIVE;
+    if (setsockopt(ListenSocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
 #else
   int reuse = 1;
-#endif
 
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
+#endif
+
+
   
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
-  
+
+#ifdef _WIN64
+    if (bind(ListenSocket, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 4221\n";
+        return 1;
+    }
+
+    int connection_backlog = 5;
+    if (listen(ListenSocket, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+
+    std::cout << "Waiting for a client to connect...\n";
+
+    accept(ListenSocket, (struct sockaddr *) &client_addr,(socklen_t *)&client_addr_len );
+    std::cout << "Client connected\n";
+
+    close(ListenSocket);
+#else
+
+
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
@@ -66,8 +114,10 @@ int main(int argc, char **argv) {
 
   accept(server_fd, (struct sockaddr *) &client_addr,(socklen_t *)&client_addr_len );
   std::cout << "Client connected\n";
-  
+
   close(server_fd);
+
+#endif
 
   return 0;
 }
