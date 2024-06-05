6865ae4e89647203268170dd92a2fcadc2549498
esmayl <esmayl@jellyfishdm.com>
2024-06-05 12:50:02 +0200

    Fixed length error on succes response on root /

--- src/server.cpp
+++ src/server.cpp
@@ -217,7 +217,7 @@ int main(int argc, char **argv)
           std::cout << "Sending success: " << newCharPointer << std::endl;
 
           // Send a 200 success response when using GET and using no endpoint
-          send(connectedClient,Globals::successResponse,strlen(Globals::successResponse),0);
+          send(connectedClient,newCharPointer,strlen(newCharPointer),0);
       }
       else if(resp.GetRequestType() == HTTPMETHOD::GET)
       {
