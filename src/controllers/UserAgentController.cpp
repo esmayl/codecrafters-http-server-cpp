3acc1c7ba5e0b3ccbfacf430b3240f54deb5fa27
esmayl <esmayl@jellyfishdm.com>
2024-08-11 13:17:41 +0200

    Fixed typo

--- src/controllers/UserAgentController.cpp
+++ src/controllers/UserAgentController.cpp
@@ -4,11 +4,11 @@
 
 #include "UserAgentController.h"
 
-void UserAgentController::SendResponse(SocketWrapper sockerWrapper, HttpPacket &packet)
+void UserAgentController::SendResponse(SocketWrapper socketWrapper, HttpPacket &packet)
 {
     std::string tempString = Globals::BuildResponse(packet.GetUserAgent(), CONTENTTYPE::PLAIN, true);
 
     std::cout << "Sending: " << tempString << std::endl;
 
-    send(sockerWrapper.socket,tempString.c_str(),static_cast<int>(tempString.length()),0);
+    send(socketWrapper.socket,tempString.c_str(),static_cast<int>(tempString.length()),0);
 }
